7cca4178b2028bf55fcef4733121e1cf287bda64
Paul-Louis NECH <pln@algolia.com>
2019-04-26 09:28:07 +0200

    feat: NLTK-based POS tags

--- turing.py
+++ turing.py
@@ -1,9 +1,29 @@
+import nltk
+
 examples = [
     "natural language sentence",
     "I am a natural language sentence"
 ]
 
 
+def download_nltk_models():
+    nltk.download("punkt")
+    nltk.download("averaged_perceptron_tagger")
+
+
+def turing(sentence):
+    tokens = nltk.word_tokenize(sentence)
+    pos_tags = nltk.pos_tag(tokens)
+    print("POS: ", pos_tags)
+    return len(sentence) / 100
+
+
+def main():
+    # download_nltk_models()
+    run_turing(examples)
+
+
+# region Helpers
 def print_results(result_pairs):
     for (example, result) in result_pairs:
         print("%s -> %s" % (example, result))
@@ -16,14 +36,6 @@ def run_turing(collection):
     print_results(result_pairs)
 
 
-def turing(string):
-    proba = len(string) / 100
-    return proba
-
-
-def main():
-    run_turing(examples)
-
-
 if __name__ == "__main__":
     main()
+# endregion
