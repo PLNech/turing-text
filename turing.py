e090184961aeeeb1c0f1e6761fcfd2244ed11686
Paul-Louis NECH <pln@algolia.com>
2019-04-26 09:50:47 +0200

    feat: Naive classifier reporting accuracy

--- turing.py
+++ turing.py
@@ -3,26 +3,25 @@ from examples import *
 
 from textblob.classifiers import NaiveBayesClassifier
 
-
-
 def download_nltk_models():
     nltk.download("punkt")
     nltk.download("averaged_perceptron_tagger")
 
 
-def turing(sentence):
+def turing_classify(sentence):
     tokens = nltk.word_tokenize(sentence)
     pos_tags = nltk.pos_tag(tokens)
-    print("POS: ", pos_tags)
-    return len(sentence) / 100
+    # print("POS: ", pos_tags)
+    score = len(sentence) / 100
+    return score >= 0.25
 
 
 def main():
     # download_nltk_models()
     print("Negatives:")
-    run_turing(examples_negatives)
+    run_turing(examples_negatives, False)
     print("\n\n\nPositives:")
-    run_turing(examples_positives)
+    run_turing(examples_positives, True)
 
 
 # region Helpers
@@ -31,11 +30,14 @@ def print_results(result_pairs):
         print("%s -> %s" % (example, result))
 
 
-def run_turing(collection):
+def run_turing(collection, expected):
     result_pairs = []
     for item in collection:
-        result_pairs.append((item, turing(item)))
-    print_results(result_pairs)
+        result_pairs.append((item, turing_classify(item)))
+    # print_results(result_pairs)
+    accuracy = sum([1 for (item, output) in result_pairs if output is expected]) / len(result_pairs)
+    print("Accuracy: %s" % accuracy)
+    return accuracy
 
 
 if __name__ == "__main__":
